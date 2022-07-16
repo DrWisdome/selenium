6df14a7eccb000dda63407f0e1335c7ea70a5717
DrWisdome <tomas.jurov@gmail.com>
2022-07-16 21:23:23 +0200

    implicitly wait - if website load faster then continues

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get('https://demo.anhtester.com/jquery-download-progress-bar-demo.html')
+driver.implicitly_wait(3)
 myElement = driver.find_element(By.ID, "downloadButton")
 myElement.click()
