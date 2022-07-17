7a5dd78ed367613e8d2ab909e558375a7a483e27
DrWisdome <tomas.jurov@gmail.com>
2022-07-17 15:20:21 +0200

    currency formatted string

--- bot/booking/booking.py
+++ bot/booking/booking.py
@@ -1,10 +1,13 @@
 import booking.constants as const
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 class Booking(webdriver.Chrome):
     def __init__(self,teardown=False):
         self.teardown = teardown
         super().__init__()
+        self.implicitly_wait(15)
+        self.maximize_window()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.teardown:
@@ -12,3 +15,23 @@ class Booking(webdriver.Chrome):
 
     def land_first_page(self):
         self.get(const.BASE_URL)
+        try:
+            self.find_element(By.ID, 'onetrust-accept-btn-handler').click()
+        except:
+            print('Didnt find the element')
+
+    def change_currency(self, currency=None):
+        currency_element = self.find_element(
+            By.CSS_SELECTOR, 'button[data-tooltip-text="Vybrať menu"]'
+        )
+        currency_element.click()
+        selector = f'a[data-modal-header-async-url-param*="selected_currency={currency}"]'
+        selected_currency_element = self.find_element(
+            By.CSS_SELECTOR, selector
+        )
+        selected_currency_element.click()
+
+    def select_place_to_go(self, place_to_go):
+        search_field = self.find_element(By.ID, 'ss')
+        search_field.clear()
+        search_field.send_keys(place_to_go)
--- bot/run.py
+++ bot/run.py
@@ -1,4 +1,7 @@
+from locale import currency
 from booking.booking import Booking
 
-with Booking(teardown=True) as bot:
+with Booking(teardown=False) as bot:
     bot.land_first_page()
+    bot.change_currency(currency='USD')
+    bot.select_place_to_go('New York')
